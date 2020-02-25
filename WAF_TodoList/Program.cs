b01e16736a2bc549cd8bd60b6794bebd28f24dfd
robi.panczel <robi.panczel@gmail.com>
2020-02-25 14:18:14 +0100

    Add project files.

---
+++ WAF_TodoList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WAF_TodoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
